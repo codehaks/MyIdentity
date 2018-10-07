67935ed3c1ab9aca2f7414880eece873d138034b
Hakim Ghods <ghods.hakim@hotmail.com>
2018-10-07 12:53:54 +0330

    Application User

--- MyApp/Startup.cs
+++ MyApp/Startup.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using MyApp.Data;
+using System;
 
 namespace MyApp
 {
@@ -13,10 +15,27 @@ namespace MyApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
             services.AddDbContext<AppDbContext>(options =>
                options.UseSqlite("Data Source=app.db"));
 
-            services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<AppDbContext>();
+            services.AddDefaultIdentity<IdentityUser>()
+                .AddEntityFrameworkStores<AppDbContext>();
+
+            services.Configure<IdentityOptions>(options =>
+            {                
+                //options.User.RequireUniqueEmail = false;
+                //options.Password.RequiredUniqueChars = 0;
+                //options.Password.RequiredLength = 6;
+                //options.SignIn.RequireConfirmedEmail = false;
+                //options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromDays(3);     
+            });
+
+            services.Configure<CookieAuthenticationOptions>(options =>
+            {               
+                options.ExpireTimeSpan = TimeSpan.FromDays(1);
+                options.Cookie.Expiration = TimeSpan.FromDays(2); 
+            });
         }
 
 
