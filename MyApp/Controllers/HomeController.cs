e49a3a6845a0c847f52040f1823aaa898269acb2
Hakim Ghods <ghods.hakim@hotmail.com>
2018-10-07 14:12:09 +0330

    Add claims

--- MyApp/Controllers/HomeController.cs
+++ MyApp/Controllers/HomeController.cs
@@ -1,15 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Security.Claims;
+using System.Threading.Tasks;
 
 namespace MyApp.Controllers
 {
     [Authorize]
     public class HomeController : Controller
     {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager, ILogger<HomeController> logger)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _logger = logger;
+        }
+
         [AllowAnonymous]
         public IActionResult Index()
         {
@@ -19,6 +31,22 @@ namespace MyApp.Controllers
         public IActionResult IdentityType()
         {
             return Ok(User.Identity.AuthenticationType);
+        }
+
+        public async Task<IActionResult> AddClaims()
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            if (user != null)
+            {
+                await _userManager.AddClaimsAsync(user
+                        , new[] {
+                            new Claim("FirstName","Jack" ?? string.Empty),
+                            new Claim("LastName","Sparrow" ?? string.Empty),
+                            new Claim("NationalCode","555" ?? string.Empty)
+                        });
+            };
+
+            return Ok();
         }
     }
 }
